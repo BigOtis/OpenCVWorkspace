6ddaa031e74826ca89dc37b39c35272d1674bb62
Phillip Lopez <pgl5711@rit.edu>
2017-02-01 16:25:20 -0500

    Changed codec to MP4

--- TestVideo/TestVideo.cpp
+++ TestVideo/TestVideo.cpp
@@ -3,7 +3,7 @@
 @brief A very basic sample for using VideoCapture and VideoWriter
 @author PkLab.net
 @modified Phillip Lopez - pgl5711@rit.edu
-@date Aug 24, 2016
+@date 2/1/2017
 */
 
 #include "stdafx.h"
@@ -16,61 +16,50 @@ using namespace std;
 
 int main(int, char**)
 {
-	Mat frame;
-	//--- INITIALIZE VIDEOCAPTURE
-	VideoCapture cap;
-	// open the default camera using default API
-	cap.open(0);
-	// OR advance usage: select any API backend
-	int deviceID = 1;             // 0 = open default camera
-	int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-								  // open selected camera using selected API
-	cap.open(deviceID + apiID);
-	// check if we succeeded
-	if (!cap.isOpened()) {
-		cerr << "ERROR! Unable to open camera\n";
+	VideoCapture cap(0); // open the default camera
+	if (!cap.isOpened()) {  // check if we succeeded
+		cout << "Unable to open the default camera";
 		return -1;
 	}
 
-	//--- GRAB AND WRITE LOOP
-	cout << "Start grabbing" << endl
-		<< "Press any key to terminate" << endl;
+	// Make the resolution bigger
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
+
+	// Video writer setup DIVX (For Windows)
+	Mat src;
+	cap >> src;
+	bool isColor = (src.type() == CV_8UC3);
+	VideoWriter writer;
+	int codec = CV_FOURCC('M', 'P', '4', 'V');  // select desired codec (must be available at runtime)
+	double fps = 25.0;                          // framerate of the created video stream
+	string filename = "./matrix_test.mp4";             // name of the output video file
+	writer.open(filename, codec, fps, src.size(), isColor);
+
+	// Find edges, display them to user, save them to a file
+	Mat edges;
+	namedWindow("edges", 1);
 	for (;;)
 	{
-		// wait for a new frame from camera and store it into 'frame'
-		cap.read(frame);
-		// check if we succeeded
-		if (frame.empty()) {
-			cerr << "ERROR! blank frame grabbed\n";
-			break;
-		}
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+		cvtColor(frame, edges, COLOR_BGR2GRAY);
 
-		// apply a blur to the frame
-		Mat dst;
-		boxFilter(frame, dst, -1, Size(4, 4));
-
-		// Find Canny edges detector
-		Mat gray;
-		cvtColor(dst, gray, COLOR_BGR2GRAY);
-
-		Mat edge1;
-		Mat cedge;
-		Canny(gray, edge1, 30, 40, 3);
-
-		Mat color;
-		cvtColor(edge1, color, CV_GRAY2BGR);
+		// Apply Canny to the new frame
+		GaussianBlur(edges, edges, Size(5, 5), 3, 1.5);
+		Canny(edges, edges, 30, 40, 3);
 
 		// Remove the B and R channels to make this look like the matrix
+		cvtColor(edges, edges, CV_GRAY2BGR);
 		Mat BGRChannels[3];
-		split(color, BGRChannels); // split the BGR channesl
-		BGRChannels[0] = Mat::zeros(color.rows, color.cols, CV_8UC1);// removing Green channel
-		BGRChannels[2] = Mat::zeros(color.rows, color.cols, CV_8UC1);// removing Green channel
-		merge(BGRChannels, 3, color); // pack the image 
-		// minor change
-		// show live and wait for a key with timeout long enough to show images
-		imshow("Live", color);
-		if (waitKey(5) >= 0)
-			break;
+		split(edges, BGRChannels); // split the BGR channesl
+		BGRChannels[0] = Mat::zeros(edges.rows, edges.cols, CV_8UC1);// removing Green channel
+		BGRChannels[2] = Mat::zeros(edges.rows, edges.cols, CV_8UC1);// removing Green channel
+		merge(BGRChannels, 3, edges); // pack the image 
+
+		writer.write(edges);
+		imshow("edges", edges);
+		if (waitKey(30) >= 0) break;
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
